90fb019d7fef1c35814d4026ed1608a1f0811e1d
Yagnesh Patel <109854114+yagneshcortex@users.noreply.github.com>
2024-06-21 09:11:21 +0530

    Update contact-form.php

--- assets/php/contact-form.php
+++ assets/php/contact-form.php
@@ -1,16 +1,29 @@
 <?php
-    $name = $_POST['name'];
-    $email = $_POST['email'];
-    $message = $_POST['message'];
-    $to = 'hello@cortexstudio.in'; //<-- Enter your E-Mail address here.
-    $subject = $_POST['subject'];
+    // Sanitize and validate input data
+    function sanitize_input($data) {
+        return htmlspecialchars(strip_tags(trim($data)));
+    }
 
-    $body = "From: $name <br> E-Mail: $email <br> Message: <br> $message";
+    $name = sanitize_input($_POST['name']);
+    $email = filter_var(sanitize_input($_POST['email']), FILTER_VALIDATE_EMAIL);
+    $subject = sanitize_input($_POST['subject']);
+    $message = sanitize_input($_POST['message']);
 
-    $headers  = 'MIME-Version: 1.0' . "\r\n";
-    $headers .= 'Content-type: text/html; charset=utf-8' . "\r\n";
-    $headers .= 'From:' . $email. "\r\n";
-    $headers .= 'Cc:' . $email. "\r\n";
+    if ($name && $email && $subject && $message) {
+        $to = 'hello@cortexstudio.in'; // <-- Enter your E-Mail address here.
+        $body = "From: $name <br> E-Mail: $email <br> Message: <br> $message";
 
-    mail($to, "New Message from Website: $subject", $body, $headers);
+        $headers  = 'MIME-Version: 1.0' . "\r\n";
+        $headers .= 'Content-type: text/html; charset=utf-8' . "\r\n";
+        $headers .= 'From:' . $email . "\r\n";
+        $headers .= 'Cc:' . $email . "\r\n";
+
+        if (mail($to, "New Message from Website: $subject", $body, $headers)) {
+            echo "success";
+        } else {
+            echo "error";
+        }
+    } else {
+        echo "error";
+    }
 ?>
